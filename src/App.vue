7d79ec69d00f1fde1ed1799516fbb2725f8c262a
Akli Reguig <akli.reguig-extern@renault.com>
2020-06-15 17:35:17 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Header />
+    <Feature />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Feature from "./components/Feature.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Feature,
+    Footer
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src="android-icon-192x192.png" width="28" height="28" />
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="/">Home</a>
+        <a class="navbar-item">Documentation</a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <!--<div class="buttons">
+            <a class="button is-light">
+              <strong>Share</strong>
+            </a>
+          </div>-->
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {}
+};
+</script>

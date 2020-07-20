329bd3000795c270eac1af86950ec007cd6cfad8
Akli Reguig <akli.reguig-extern@renault.com>
2020-07-20 10:40:53 +0200

    Fix: Home button url in header.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="/">
+      <a class="navbar-item" href="/kaas-ui">
         <img src="android-icon-192x192.png" width="28" height="28" />
         Karate As A Service
       </a>
@@ -21,7 +21,7 @@
 
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
-        <a class="navbar-item" href="/">Home</a>
+        <a class="navbar-item" href="/kaas-ui">Home</a>
         <a class="navbar-item" href="/kaas-doc" target="_blank">Documentation</a>
       </div>
 
